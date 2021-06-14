06ebfdb024afe0d85cea353744e409fdc7b5a607
jamesdoh0109 <magicjcdp@gmail.com>
2021-06-14 17:20:00 +0900

    layout for login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,8 +3,21 @@ import React, { Component } from 'react'
 export default class Login extends Component {
     render() {
         return (
-            <div>
-                
+            <div className="form-signin text-center" style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>
+                <form style={{width: '300px'}}>
+                    <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
+                    <div className="form-floating">
+                        <input type="email" className="form-control" placeholder="Email"/>
+                        <label>Email address</label>
+                    </div>
+                    <div className="form-floating">
+                        <input type="password" className="form-control" placeholder="Password"/>
+                        <label>Password</label>
+                    </div>
+                    <div className="mt-3">
+                        <button className="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+                    </div>
+                </form>                    
             </div>
         )
     }
